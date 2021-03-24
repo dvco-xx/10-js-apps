1d04fe3ba874a5703df584356b5598fbc38b4d4d
David Osuchukwu <osuchukwudavid@gmail.com>
2021-03-24 14:26:10 +0100

    added localstorage to store activities. finished todo-app

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,28 +1,68 @@
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-const todos = document.getElementById('todos');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUl = document.getElementById("todos");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+const todos = JSON.parse(localStorage.getItem("todo"));
 
-    const todoText = input.value;
+if (todos) {
+  todos.forEach((todo) => {
+    addToDo(todo);
+  });
+}
 
-    if(todoText) {
-        const todoEl = document.createElement('li');
-        todoEl.innerHTML = todoText;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-        todoEl.addEventListener('click', (e) => {
-            todoEl.classList.toggle('completed');
-        });
-
-        todoEl.addEventListener('contextmenu', (e) => {
-            e.preventDefault();
-
-            todoEl.remove();
-        })
-
-        todos.appendChild(todoEl);
-
-        input.value = '';
-    }
+  addToDo();
 });
+
+function addToDo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = input.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerHTML = todoText;
+
+    todoEl.addEventListener("click", (e) => {
+      todoEl.classList.toggle("completed");
+
+      updateLS();
+    });
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+
+      updateLS();
+    });
+
+    todosUl.appendChild(todoEl);
+    input.value = "";
+
+    updateLS();
+  }
+}
+
+function updateLS() {
+  const todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
